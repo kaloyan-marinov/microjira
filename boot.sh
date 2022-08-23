29ba3b69785b4d1dd95120b82d66d521e5043023
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-08-23 21:11:01 +0200

    03: (part 09 of tutorial) attempt to fix the issues that continue occurring (by commenting out the db-related code from `boot.sh`)

--- boot.sh
+++ boot.sh
@@ -2,17 +2,17 @@
 
 source venv/bin/activate
 
-while true; do
-    echo "trying to execute the database-migration scripts"
-    flask db upgrade
+# while true; do
+#     echo "trying to execute the database-migration scripts"
+#     flask db upgrade
 
-    if [[ "$?" == "0" ]]; then
-        echo "- succeeded!"
-        break
-    fi
+#     if [[ "$?" == "0" ]]; then
+#         echo "- succeeded!"
+#         break
+#     fi
 
-    echo "- failed; wait 5 seconds before re-trying..."
-    sleep 5
-done
+#     echo "- failed; wait 5 seconds before re-trying..."
+#     sleep 5
+# done
 
-exec gunicorn -b :8000 --access-logfile - --error-logfile - application:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - application:app
